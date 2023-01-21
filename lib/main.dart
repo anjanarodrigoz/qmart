da7c11b011903ce68c5e140b643e141c5b975431
anjanarodrigoz <85009096+anjanarodrigoz@users.noreply.github.com>
2023-01-22 03:23:50 +0530

    login screen created

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,8 @@
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/get_navigation.dart';
 import 'package:qmart/pages/login_page.dart';
 
-import 'Animation/FadeAnimation.dart';
 import 'firebase_options.dart';
 
 void main() async {
@@ -12,7 +10,6 @@ void main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
-  await FirebaseDatabase.instance.ref().child('name').set('Anjana');
   runApp(const GetMaterialApp(
     debugShowCheckedModeBanner: false,
     home: LoginPage(),
