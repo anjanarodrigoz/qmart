8e317a97627de20df0bc58031f3b338037ff47ee
anjanarodrigoz <85009096+anjanarodrigoz@users.noreply.github.com>
2023-02-14 14:17:36 +0530

    update home page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -13,40 +13,58 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       body: Center(
         child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-          SizedBox(
-            height: 50.0,
-            child: ElevatedButton(
-              onPressed: () {
-                Get.to(() => const ShopPage());
-              },
-              child: const Text(
-                'New Purchase',
-                style: TextStyle(fontSize: 20.0),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0),
+            child: SizedBox(
+              height: 50.0,
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () {
+                  Get.to(() => const ShopPage());
+                },
+                child: const Text(
+                  'New Purchase',
+                  style: TextStyle(fontSize: 20.0),
+                ),
               ),
             ),
           ),
           const SizedBox(
             height: 50.0,
           ),
-          SizedBox(
-            height: 50.0,
-            child: ElevatedButton(
-              onPressed: () {
-                Get.to(() => const HistoryPage());
-              },
-              child: const Text(
-                'Purchased History',
-                style: TextStyle(fontSize: 20.0),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0),
+            child: SizedBox(
+              height: 50.0,
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () {
+                  Get.to(() => const HistoryPage());
+                },
+                child: const Text(
+                  'Purchased History',
+                  style: TextStyle(fontSize: 20.0),
+                ),
               ),
             ),
           ),
           const SizedBox(height: 50.0),
-          ElevatedButton(
-            style: ElevatedButton.styleFrom(backgroundColor: Colors.red),
-            onPressed: () {
-              Get.to(() => const LoginPage());
-            },
-            child: const Text('Sign out'),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0),
+            child: SizedBox(
+              height: 50.0,
+              width: double.infinity,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(backgroundColor: Colors.red),
+                onPressed: () {
+                  Get.to(() => const LoginPage());
+                },
+                child: const Text(
+                  'Sign out',
+                  style: TextStyle(fontSize: 20.0),
+                ),
+              ),
+            ),
           )
         ]),
       ),
