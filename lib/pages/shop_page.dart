ff33b41ba229ac32b1192494c3609129027ee07c
anjanarodrigoz <85009096+anjanarodrigoz@users.noreply.github.com>
2023-02-17 02:06:34 +0530

    update shop card

--- lib/pages/shop_page.dart
+++ lib/pages/shop_page.dart
@@ -19,22 +19,36 @@ class _ShopPageState extends State<ShopPage> {
       'rateCount': '15',
       'miles': '0.4',
       'duration': '45-60'
+    },
+    '10002': {
+      'id': '1001',
+      'name': 'Arukkaru',
+      'url':
+          'https://www.here.com/sites/g/files/odxslz256/files/2022-06/mining-truck-mine-blog.jpg',
+      'rate': '4.6',
+      'rateCount': '15',
+      'miles': '0.4',
+      'duration': '45-60'
     }
   };
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(),
-        body: Container(
-          padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-          height: 400.0,
-          child: ListView.builder(
-            itemCount: map.values.toList().length,
-            itemBuilder: (BuildContext context, int index) {
-              return ShopCard();
-            },
-          ),
+        appBar: AppBar(
+          backgroundColor: Colors.amber.shade900,
+        ),
+        body: ListView.builder(
+          itemCount: map.values.toList().length,
+          padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 10.0),
+          itemBuilder: (BuildContext context, int index) {
+            return ShopCard(
+              addToFavourite: () {},
+              description: 'Siyaluma Gal kapana wada karanu labe',
+              name: 'Randima Gal quarry',
+              onTap: () {},
+            );
+          },
         ));
   }
 }
--- lib/widgets/shop_card.dart
+++ lib/widgets/shop_card.dart
@@ -2,170 +2,196 @@ import 'package:flutter/material.dart';
 import 'package:image_network/image_network.dart';
 
 class ShopCard extends StatelessWidget {
+  final String name;
+  final String description;
+  final double miles;
+  final String url;
+  final double rating;
+  final int ratingCount;
+  final String duration;
+  final VoidCallback onTap;
+  final VoidCallback addToFavourite;
+
+  const ShopCard(
+      {super.key,
+      required this.name,
+      required this.description,
+      this.url =
+          'https://www.mining.com/wp-content/uploads/2016/10/a-lot-more-automation-a-lot-less-humans-predicted-for-the-mining-industry.jpg',
+      this.miles = 0.5,
+      this.rating = 5.0,
+      this.ratingCount = 10,
+      this.duration = '30-35',
+      required this.onTap,
+      required this.addToFavourite});
+
   @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
     return SizedBox(
       width: size.width,
-      child: Card(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Stack(
-              clipBehavior: Clip.none,
-              children: [
-                GestureDetector(
-                  onTap: () {},
-                  child: SizedBox(
-                      width: size.width,
-                      height: 180,
-                      child: ImageNetwork(
-                        image:
-                            'https://www.here.com/sites/g/files/odxslz256/files/2022-06/mining-truck-mine-blog.jpg',
-                        width: double.infinity,
-                        height: double.infinity,
-                        duration: 1500,
-                        curve: Curves.bounceIn,
-                        onPointer: true,
-                        debugPrint: false,
-                        fullScreen: false,
-                        fitAndroidIos: BoxFit.fill,
-                        fitWeb: BoxFitWeb.cover,
-                        onLoading: const CircularProgressIndicator(
-                          color: Colors.black,
-                        ),
-                        onError: const Icon(
-                          Icons.error,
-                          color: Colors.red,
-                        ),
-                        onTap: () {
-                          debugPrint("©gabriel_patrick_souza");
-                        },
-                      )),
-                ),
-                Positioned(
-                  bottom: 0,
-                  right: 0,
-                  child: IconButton(
-                    color: Colors.white,
-                    icon: const Icon(Icons.favorite_border_outlined),
-                    onPressed: () {},
+      child: Padding(
+        padding: const EdgeInsets.only(bottom: 15.0),
+        child: Card(
+          elevation: 10.0,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Stack(
+                clipBehavior: Clip.none,
+                children: [
+                  GestureDetector(
+                    onTap: onTap,
+                    child: SizedBox(
+                        width: size.width,
+                        height: 180,
+                        child: ImageNetwork(
+                          image: url,
+                          width: double.infinity,
+                          height: double.infinity,
+                          duration: 1500,
+                          curve: Curves.bounceIn,
+                          onPointer: true,
+                          debugPrint: false,
+                          fullScreen: false,
+                          fitAndroidIos: BoxFit.fill,
+                          fitWeb: BoxFitWeb.cover,
+                          onLoading: const CircularProgressIndicator(
+                            color: Colors.black,
+                          ),
+                          onError: const Icon(
+                            Icons.error,
+                            color: Colors.red,
+                          ),
+                          onTap: () {
+                            debugPrint("©gabriel_patrick_souza");
+                          },
+                        )),
                   ),
-                ),
-                Positioned(
-                    top: 10,
-                    left: 10,
-                    child: Row(
-                      children: [
-                        Container(
-                          decoration: BoxDecoration(
-                              color: Colors.grey.shade300,
-                              borderRadius: BorderRadius.circular(3)),
-                          child: const Padding(
-                            padding: EdgeInsets.all(5.0),
-                            child: Text(
-                              '0.4 Mi',
-                            ),
-                          ),
-                        ),
-                        const SizedBox(
-                          width: 8,
-                        ),
-                        Container(
-                          decoration: BoxDecoration(
-                              color: Colors.grey.shade300,
-                              borderRadius: BorderRadius.circular(3)),
-                          child: Padding(
-                            padding: const EdgeInsets.all(5.0),
-                            child: Row(
-                              children: const [
-                                Text(
-                                  '4.6',
-                                  style: TextStyle(
-                                    fontSize: 14,
-                                  ),
-                                ),
-                                Icon(
-                                  Icons.star_half,
-                                  size: 15.0,
-                                ),
-                                Text(
-                                  '(15)',
-                                  style: TextStyle(
-                                    fontSize: 14,
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                        const SizedBox(
-                          width: 8,
-                        ),
-                        Container(
-                          decoration: BoxDecoration(
-                              color: Colors.grey.shade300,
-                              borderRadius: BorderRadius.circular(3)),
-                          child: const Padding(
-                            padding: EdgeInsets.all(5.0),
-                            child: Text(
-                              '45-60 min',
-                              style: TextStyle(
-                                fontSize: 14,
+                  Positioned(
+                    bottom: 0,
+                    right: 0,
+                    child: IconButton(
+                      color: Colors.white,
+                      icon: const Icon(Icons.favorite_border_outlined),
+                      onPressed: addToFavourite,
+                    ),
+                  ),
+                  Positioned(
+                      top: 10,
+                      left: 10,
+                      child: Row(
+                        children: [
+                          Container(
+                            decoration: BoxDecoration(
+                                color: Colors.grey.shade300,
+                                borderRadius: BorderRadius.circular(3)),
+                            child: Padding(
+                              padding: const EdgeInsets.all(5.0),
+                              child: Text(
+                                '$miles Mi',
                               ),
                             ),
                           ),
-                        ),
-                      ],
-                    )),
-              ],
-            ),
-            Padding(
-              padding:
-                  const EdgeInsets.symmetric(vertical: 2.0, horizontal: 10.0),
-              child: Text(
-                'Name',
-                style: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.w500,
-                    color: Colors.green.shade900),
+                          const SizedBox(
+                            width: 8,
+                          ),
+                          Container(
+                            decoration: BoxDecoration(
+                                color: Colors.grey.shade300,
+                                borderRadius: BorderRadius.circular(3)),
+                            child: Padding(
+                              padding: const EdgeInsets.all(5.0),
+                              child: Row(
+                                children: [
+                                  Text(
+                                    '$rating',
+                                    style: const TextStyle(
+                                      fontSize: 14,
+                                    ),
+                                  ),
+                                  const Icon(
+                                    Icons.star_half,
+                                    size: 15.0,
+                                  ),
+                                  Text(
+                                    '($ratingCount)',
+                                    style: const TextStyle(
+                                      fontSize: 14,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                          const SizedBox(
+                            width: 8,
+                          ),
+                          Container(
+                            decoration: BoxDecoration(
+                                color: Colors.grey.shade300,
+                                borderRadius: BorderRadius.circular(3)),
+                            child: Padding(
+                              padding: const EdgeInsets.all(5.0),
+                              child: Text(
+                                '$duration min',
+                                style: const TextStyle(
+                                  fontSize: 14,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      )),
+                ],
               ),
-            ),
-            const Padding(
-              padding: EdgeInsets.only(left: 10.0),
-              child: Text(
-                'description',
-                style: TextStyle(
-                  fontSize: 14,
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 2.0, horizontal: 10.0),
+                child: Text(
+                  name,
+                  style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.green.shade900),
                 ),
               ),
-            ),
-            const SizedBox(
-              height: 10,
-            )
-            // Padding(
-            //   padding:
-            //       const EdgeInsets.symmetric(vertical: 2.0, horizontal: 10.0),
-            //   child: Row(
-            //     children: const [
-            //       Text(
-            //         "Sponsored",
-            //         style: TextStyle(
-            //           fontSize: 14,
-            //         ),
-            //       ),
-            //       SizedBox(
-            //         width: 5,
-            //       ),
-            //       Icon(
-            //         Icons.info,
-            //         color: Colors.grey,
-            //         size: 15,
-            //       )
-            //     ],
-            //   ),
-            // ),
-          ],
+              Padding(
+                padding: const EdgeInsets.only(left: 10.0),
+                child: Text(
+                  description,
+                  style: const TextStyle(
+                    fontSize: 14,
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              )
+              // Padding(
+              //   padding:
+              //       const EdgeInsets.symmetric(vertical: 2.0, horizontal: 10.0),
+              //   child: Row(
+              //     children: const [
+              //       Text(
+              //         "Sponsored",
+              //         style: TextStyle(
+              //           fontSize: 14,
+              //         ),
+              //       ),
+              //       SizedBox(
+              //         width: 5,
+              //       ),
+              //       Icon(
+              //         Icons.info,
+              //         color: Colors.grey,
+              //         size: 15,
+              //       )
+              //     ],
+              //   ),
+              // ),
+            ],
+          ),
         ),
       ),
     );
