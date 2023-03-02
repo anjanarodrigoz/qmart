02817597289043d03bd9f6e97be6d9d51e537b7a
anjanarodrigoz <85009096+anjanarodrigoz@users.noreply.github.com>
2023-03-02 12:32:21 +0530

    Update shop_browsing_page.dart

--- lib/user-app/shop_browsing_page.dart
+++ lib/user-app/shop_browsing_page.dart
@@ -35,9 +35,7 @@ class _ShopBrowsingPageState extends State<ShopBrowsingPage> {
               rating: quarry['rate'],
               ratingCount: quarry['rateCount'] as int,
               url: quarry['url'],
-              onTap: () {
-                Get.to(QuarryMaterialsPage());
-              },
+              onTap: () {},
             );
           },
         ));
@@ -48,9 +46,9 @@ class _ShopBrowsingPageState extends State<ShopBrowsingPage> {
   final map = {
     '10001': {
       'id': '1001',
-      'name': 'Galapatha',
+      'name': 'Access',
       'url':
-          'https://www.here.com/sites/g/files/odxslz256/files/2022-06/mining-truck-mine-blog.jpg',
+          'https://www.google.com/search?q=access+engineering+plc&sxsrf=AJOqlzVdjqK1iG9rH35zcEaAodc4ro-dKw:1677696476627&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjK3J60srv9AhWT-XMBHaYvCNEQ_AUoA3oECAEQBQ&biw=958&bih=908&dpr=1#imgrc=0s7k3wJcCu0_oM ',
       'rate': 4.6,
       'rateCount': 15,
       'miles': 0.4,
@@ -59,9 +57,9 @@ class _ShopBrowsingPageState extends State<ShopBrowsingPage> {
     },
     '10002': {
       'id': '1001',
-      'name': 'Arukkaru',
+      'name': 'Metal Mix',
       'url':
-          'https://www.mining.com/wp-content/uploads/2016/10/a-lot-more-automation-a-lot-less-humans-predicted-for-the-mining-industry.jpg',
+          'https://www.google.com/search?q=metal+mix+quarry&tbm=isch&ved=2ahUKEwiPsrHDsrv9AhWy03MBHZekBtEQ2-cCegQIABAA&oq=metal+mix+quarry&gs_lcp=CgNpbWcQAzoECCMQJzoFCAAQgAQ6BAgAEB46BggAEAgQHjoHCAAQgAQQGFCiBViRGGCeH2gAcAB4AIABS4gBuwSSAQE4mAEAoAEBqgELZ3dzLXdpei1pbWfAAQE&sclient=img&ei=_J3_Y4_EF7Knz7sPl8maiA0&bih=908&biw=958#imgrc=WbwKB9_6wlpPGM ',
       'rate': 4.6,
       'rateCount': 15,
       'miles': 0.4,
@@ -70,9 +68,9 @@ class _ShopBrowsingPageState extends State<ShopBrowsingPage> {
     },
     '10003': {
       'id': '1001',
-      'name': 'Arukkaru',
+      'name': 'ICC',
       'url':
-          'https://www.here.com/sites/g/files/odxslz256/files/2022-06/mining-truck-mine-blog.jpg',
+          'https://www.google.com/url?sa=i&url=https%3A%2F%2Ficc-construct.com%2Fproperty-development%2Fmount-clifford-residencies&psig=AOvVaw3QMs8NwkPSy-gex-VryPXH&ust=1677782968069000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCKCt3-Cyu_0CFQAAAAAdAAAAABAE ',
       'rate': 4.6,
       'rateCount': 15,
       'miles': 0.4,
@@ -80,17 +78,6 @@ class _ShopBrowsingPageState extends State<ShopBrowsingPage> {
       'description': 'Largest limestone quarry',
     },
     '10004': {
-      'id': '1001',
-      'name': 'Arukkaru',
-      'url':
-          'https://www.here.com/sites/g/files/odxslz256/files/2022-06/mining-truck-mine-blog.jpg',
-      'rate': 4.6,
-      'rateCount': 15,
-      'miles': 0.4,
-      'duration': '45-60',
-      'description': 'Largest limestone quarry',
-    },
-    '10005': {
       'id': '1001',
       'name': 'Arukkaru',
       'url':
